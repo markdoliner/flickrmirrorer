70cedae33726819011f69d494daad9794ca662e4
Victor Engmark <victor.engmark@gmail.com>
2016-12-25 12:36:43 +0000

    Extract tests to a separate file

--- flickrmirrorer.py
+++ flickrmirrorer.py
@@ -127,35 +127,7 @@ def _validate_json_response(rsp):
         sys.exit(1)
 
 
-def test_known_timestamp():
-    timestamp = _get_timestamp({
-        'datetakenunknown': '0',
-        'datetaken': '2015-11-02 12:35:07'
-    })
-    assert timestamp.isoformat() == "2015-11-02T12:35:07"
-
-
-def test_plain_title_timestamp():
-    timestamp = _get_timestamp({
-        'datetakenunknown': '1',
-        'datetaken': '2014-10-01 13:45:37',
-        'title': '20151130_135610'
-    })
-    assert timestamp.isoformat() == "2015-11-30T13:56:10"
-
-
-def test_unparseable_title_timestamp():
-    timestamp = _get_timestamp({
-        'datetakenunknown': '1',
-        'datetaken': '2014-10-01 13:45:37',
-        'title': 'flaskpost'
-    })
-
-    # Fall back on datetaken if we can't parse the date from the title
-    assert timestamp.isoformat() == "2014-10-01T13:45:37"
-
-
-def _get_timestamp(photo):
+def get_timestamp(photo):
     """
     Return photo timestamp, get it from:
     1. datetaken unless datetakenunknown
@@ -386,7 +358,7 @@ class FlickrMirrorer(object):
                 'Skipping metadata for %s because we already have it' %
                 photo_basename)
 
-        timestamp = _get_timestamp(photo)
+        timestamp = get_timestamp(photo)
         self._set_timestamp_if_different(timestamp, photo_filename)
         self._set_timestamp_if_different(timestamp, metadata_filename)
 
---
+++ tests.py
@@ -0,0 +1,29 @@
+from .flickrmirrorer import get_timestamp
+
+
+def test_unparseable_title_timestamp():
+    timestamp = get_timestamp({
+        'datetakenunknown': '1',
+        'datetaken': '2014-10-01 13:45:37',
+        'title': 'flaskpost'
+    })
+
+    # Fall back on datetaken if we can't parse the date from the title
+    assert timestamp.isoformat() == "2014-10-01T13:45:37"
+
+
+def test_plain_title_timestamp():
+    timestamp = get_timestamp({
+        'datetakenunknown': '1',
+        'datetaken': '2014-10-01 13:45:37',
+        'title': '20151130_135610'
+    })
+    assert timestamp.isoformat() == "2015-11-30T13:56:10"
+
+
+def test_known_timestamp():
+    timestamp = get_timestamp({
+        'datetakenunknown': '0',
+        'datetaken': '2015-11-02 12:35:07'
+    })
+    assert timestamp.isoformat() == "2015-11-02T12:35:07"
